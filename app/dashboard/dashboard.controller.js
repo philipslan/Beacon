f9db3e3c368bb9b714b7e15d89094f051e396f8a
Philip Lan <philiplan2017@u.northwestern.edu>
2016-12-18 00:39:48 -0500

    add your profile to your friends on account creation

--- app/dashboard/dashboard.controller.js
+++ app/dashboard/dashboard.controller.js
@@ -1,10 +1,11 @@
 (function(){
     angular.module("beacon")
     .controller("DashboardController",["$scope", "$http", function($scope, $http){
-        $scope.logout = function () {
+        $scope.logout = function (e) {
             $http.post("/auth/logout").success(function(){
                 window.location.assign("/");
             });
+            e.preventDefault();
         }
     }]);
 }());
