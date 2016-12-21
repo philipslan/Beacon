9063bf91c046a63e2a3fbf90b59e2ad964308b99
Christopher Li <Christopherli2017@u.northwestern.edu>
2016-12-21 03:56:09 -0600

    event creation urls now work correctly

--- app/createEvent/createEvent.controller.js
+++ app/createEvent/createEvent.controller.js
@@ -3,18 +3,58 @@
     .factory("createEventFactory", function() {
     	var factory = {};
     	var form = {};
-    	factory.getForm = function() {
-    		return form;
-    	};
+
     	factory.appendForm = function(inputForm) {
-    		form = extend(form, inputForm);
+    		for (i in inputForm) {
+    			form[i] = inputForm[i]
+    		}
+    		return form;
     	};
     	return factory;
     })
-    .controller("CreateEventController",["$scope", "createEventFactory", function($scope, createEventFactory){
-    	$scope.createEvent = function() {
-    		var form = createEventFactory.getForm();
-    		debugger;
+    .controller("CreateEventController",
+    		["$scope", 
+    		"createEventFactory", 
+    		"$location", 
+    		"$anchorScroll", 
+    		function($scope, createEventFactory, $location, $anchorScroll){
+    	var form = {};
+    	$scope.init = function() {
+    		var url = $location.url().toString().split('#');
+    		var object;
+    		if (url.length != 1) { 
+				object = $('#' +url[url.length - 1]);
+			}
+			if (object == null || object.length != 1) {
+				object = $('#type')	
+			}
+			object.addClass('active in');
+
     	}
+    	$scope.createEvent = function(inputForm) {
+    		form = createEventFactory.appendForm(inputForm);
+    	};
+    	$scope.addField =  function(inputForm) {
+    		form = createEventFactory.appendForm(inputForm);
+    	};
+    	$scope.addType = function(inputForm) {
+    		if (!inputForm) return;
+    		form = createEventFactory.appendForm(inputForm);
+    		$scope.changeTab('type','description');
+    	};
+    	$scope.addDescription = function(inputForm) {
+    		if (!inputForm) return;
+    		form = createEventFactory.appendForm(inputForm);
+    		$scope.changeTab('description','time');
+    	};
+    	$scope.addTime = function(inputForm) {
+    		if (!inputForm) return;
+    		form = createEventFactory.appendForm(inputForm);
+    		$scope.changeTab('time','friends');
+    	};
+    	$scope.changeTab = function(current, next) {
+    		$('#' + current).removeClass('active in')
+    		$('#' + next).addClass('active in');	
+    	};
     }]);
 }());
